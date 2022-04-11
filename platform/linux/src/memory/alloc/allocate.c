7cf10bade44de0a1e29ac165d70a55086aaff80f
Iris System <iris@iris.ac.nz>
2022-04-11 01:04:19 +0000

    linux/alloc: don't use MAP_ANONYMOUS

--- platform/linux/src/memory/alloc/allocate.c
+++ platform/linux/src/memory/alloc/allocate.c
@@ -4,7 +4,7 @@
 void *MemoryAllocate__raw
 (size_t len)
 {
-	void *ret = mmap(0, len, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	void *ret = mmap(0, len, PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
 	if (ret == MAP_FAILED)
 	{
 		return NULL;
